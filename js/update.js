bff5eba923477c73995e48fe26b0aa584b27735e
Siphesihle Sehume <siphosihlesehume@gmail.com>
2026-09-08 22:54:49 +0200

    IX: corrected innerHTML logic

--- js/update.js
+++ js/update.js
@@ -3,5 +3,5 @@ var unorderedList = document.getElementById("languages");
 unorderedList.onclick = function(event) {
     let language = event.target.innerHTML;
     let selection = document.getElementById("selection");
-    selection.innerHTML = "You chooose a language";
+    selection.innerHTML = "You chose " + language + " as a programming language";
 }
